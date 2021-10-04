0f22ef167a54757436c2362c27ac63042ad43baf
ThanGrove <than@virginia.edu>
2021-10-04 14:31:26 -0400

    updates to thdl styles

--- functions.php
+++ functions.php
@@ -16,11 +16,20 @@ define( 'CHILD_THEME_MANDALA_ASTRA_VERSION', '1.0.0' );
 /**
  * Enqueue styles
  */
-function child_enqueue_styles() {
-
+function thdl_custom_styles() {
 	wp_enqueue_style( 'mandala-astra-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_MANDALA_ASTRA_VERSION, 'all' );
 	wp_enqueue_style ('mandala-astra-child-css', get_stylesheet_directory_uri() .'/css/thdl-astra.css', array());
-
 }
 
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action( 'wp_enqueue_scripts', 'thdl_custom_styles', 15 );
+
+
+/**
+ * Enqueue styles
+ */
+
+function thdl_custom_js() {
+	wp_enqueue_script( 'thdl_custom_js', get_stylesheet_directory_uri() . '/js/thdl-astra.js', array('jquery'), '1.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'thdl_custom_js', 15 );
