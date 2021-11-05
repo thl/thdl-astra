1e86544fb76793f304dd523d7563dd01fee9e826
ThanGrove <than@virginia.edu>
2021-11-05 17:29:47 -0400

    taking out debug code again

--- page.php
+++ page.php
@@ -30,20 +30,20 @@ get_header(); ?>
         <div id="thdl-col-1" class="thdl-col">Im col 1</div>
 
         <div id="thdl-col-2" class="thdl-col 2ndcol">
-            <div id="mandalaroot">[I AM HERE]</div>
+
 	        <?php if ( is_active_sidebar( 'content-top-widget-area' ) ) : ?>
                 <div id="thdl-content-top" class="widget-area">
 			        <?php dynamic_sidebar( 'content-top-widget-area' ); ?>
                 </div>
 	        <?php endif; ?>
 
-            <div>
+            <div id="mandala-root"></div>
             <?php astra_primary_content_top(); ?>
 
             <?php astra_content_page_loop(); ?>
 
             <?php astra_primary_content_bottom(); ?>
-            </div>
+
         </div>
 
         <div id="thdl-col-3" class="thdl-col">
