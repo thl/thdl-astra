e68de3534f161de753f3d538a933856009548d68
ThanGrove <than@virginia.edu>
2021-11-05 17:11:42 -0400

    updating template for thl pages

--- functions.php
+++ functions.php
@@ -33,10 +33,37 @@ function thdl_custom_js() {
 
 add_action( 'wp_enqueue_scripts', 'thdl_custom_js', 15 );
 
-/*
-function thdl_test() {
-	$tp = get_page_template();
-}
+/**
+ * Custom Widget Areas for Content Top
+ * Taken from: https://qodeinteractive.com/magazine/add-widget-area-to-wordpress/
+ */
 
-add_action( 'astra_primary_content_top', 'thdl_test');
-*/
+function register_thdl_areas() {
+	// Content Top Widget Area
+	register_sidebar(
+		array(
+			'id' => 'content-top-widget-area',
+			'name' => esc_html__( 'Content Top Widget Area', 'theme-domain' ),
+			'description' => esc_html__( 'A new widget area to appear in the center column above all content including mandala pages', 'theme-domain' ),
+			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+			'after_widget' => '</div>',
+			'before_title' => '<div class="top-widget-title-holder"><h3 class="top-widget-title">',
+			'after_title' => '</h3></div>'
+		)
+	);
+}
+add_action( 'widgets_init', 'register_thdl_areas' );
+
+/**
+ * Trim the paragraph tags from the custom shortcodes in blocks
+ * @param $content
+ *
+ * @return array|string|string[]
+ *
+ */
+function thdl_clean_block_shortcodes($content){
+	$content = str_replace('<p>[madvsearch]</p>', '[madvsearch]', $content);
+	$content = str_replace('<p>[mandalaroot]</p>', '[mandalaroot]', $content);
+	return $content;
+}
+add_filter('widget_block_content', 'thdl_clean_block_shortcodes');
---
+++ page.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+        <div id="thdl-col-1" class="thdl-col"></div>
+
+        <div id="thdl-col-2" class="thdl-col">
+	        <?php if ( is_active_sidebar( 'content-top-widget-area' ) ) : ?>
+                <div id="thdl-content-top" class="widget-area">
+			        <?php dynamic_sidebar( 'content-top-widget-area' ); ?>
+                </div>
+	        <?php endif; ?>
+
+            <div id="mandala-root">
+
+            </div>
+
+            <?php astra_primary_content_top(); ?>
+
+            <?php astra_content_page_loop(); ?>
+
+            <?php astra_primary_content_bottom(); ?>
+        </div>
+
+        <div id="thdl-col-3" class="thdl-col">
+            <div id="advancedSearchPortal"></div>
+        </div>
+
+	</div><!-- #primary -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
